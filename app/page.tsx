32f481d809c0cbe20d0d8e0d60c2648dbf297172
YashwantGawande <gawandeyashwant7@gmail.com>
2023-10-20 21:38:42 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <Button>page</Button>
+  )
+}
+
+export default page
