0753bcee789667a1b34807943ba5eabfdb6f5861
YashwantGawande <gawandeyashwant7@gmail.com>
2023-10-26 23:54:46 +0530

    add site header

---
+++ components/site-header.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Navbar } from "./site-nav";
+import { ModeToggle } from "./mode-toggle";
+import { Separator } from "./ui/separator";
+
+type Props = {};
+
+const Header = (props: Props) => {
+  return (
+    <header className="sticky top-0 z-50">
+      <div className="flex items-center justify-between p-4">
+        <Navbar />
+        <ModeToggle />
+      </div>
+      <Separator className="w-full"/>
+    </header>
+  );
+};
+
+export default Header;
