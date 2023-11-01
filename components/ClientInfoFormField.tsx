c2f663f970593a7a6b829f69fe1fe4b5662068be
YashwantGawande <gawandeyashwant7@gmail.com>
2023-11-01 23:46:34 +0530

    add client info form

---
+++ components/ClientInfoFormField.tsx
@@ -0,0 +1,141 @@
+import React from "react";
+import { FormControl, FormField, FormItem } from "./ui/form";
+import { SmallInput } from "./ui/input";
+import { Control } from "react-hook-form";
+import { Invoice } from "./MainForm";
+
+interface FormProps {
+  control: Control<Invoice>;
+}
+interface ClientInfoFormField {
+  form: FormProps;
+}
+
+const ClientInfoFormField: React.FC<ClientInfoFormField> = ({ form }) => {
+  return (
+    <div>
+      <FormField
+        control={form.control}
+        name={`clients.${0}.name`}
+        render={({ field }) => (
+          <FormItem>
+            <FormControl>
+              <SmallInput
+                placeholder="Company Name"
+                {...field}
+                className="capitalize"
+              />
+            </FormControl>
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name={`clients.${0}.phoneNumber`}
+        render={({ field }) => (
+          <FormItem>
+            <FormControl>
+              <SmallInput
+                placeholder="Phone number"
+                {...field}
+                className="capitalize"
+              />
+            </FormControl>
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name={`clients.${0}.email`}
+        render={({ field }) => (
+          <FormItem>
+            <FormControl>
+              <SmallInput
+                placeholder="Email"
+                {...field}
+                className="capitalize"
+              />
+            </FormControl>
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name={`clients.${0}.address`}
+        render={({ field }) => (
+          <FormItem>
+            <FormControl>
+              <SmallInput
+                placeholder="Address"
+                {...field}
+                className="capitalize"
+              />
+            </FormControl>
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name={`clients.${0}.city`}
+        render={({ field }) => (
+          <FormItem>
+            <FormControl>
+              <SmallInput
+                placeholder="City"
+                {...field}
+                className="capitalize"
+              />
+            </FormControl>
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name={`clients.${0}.state`}
+        render={({ field }) => (
+          <FormItem>
+            <FormControl>
+              <SmallInput
+                placeholder="State"
+                {...field}
+                className="capitalize"
+              />
+            </FormControl>
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name={`clients.${0}.zipCode`}
+        render={({ field }) => (
+          <FormItem>
+            <FormControl>
+              <SmallInput
+                placeholder="Zip Code"
+                {...field}
+                className="capitalize"
+              />
+            </FormControl>
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name={`clients.${0}.country`}
+        render={({ field }) => (
+          <FormItem>
+            <FormControl>
+              <SmallInput
+                placeholder="Country"
+                {...field}
+                className="capitalize"
+              />
+            </FormControl>
+          </FormItem>
+        )}
+      />
+    </div>
+  );
+};
+
+export default ClientInfoFormField;
